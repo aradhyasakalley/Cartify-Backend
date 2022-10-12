38b7898ac2fbbb4af2ecbc7c66ead28cffd7d6f4
Smit-Dama <smitdama09@gmail.com>
2022-10-12 13:25:48 +0530

    Database and Server Setup Complete

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan=require('morgan');
+const app = express();
+const dbURL= 'mongodb+srv://smitdama09:Ralip@029@ecommerce.gykzgiy.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(dbURL , {useNewUrlParser : true , useUnifiedTopology:true})
+    .then(()=>app.listen(3000, async()=>{
+        try {
+            console.log('Connected to Database')
+            console.log('Connected to Port 3000');
+        } catch (error) {
+            console.log('Error Occured' ,error);
+        }
+    })
+    )
+    .catch((err)=>console.log(err));
+    
+
